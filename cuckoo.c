bf0db412633daadba8687ebff741fe691025031c
numberonepikey <63299377+numberonepikey@users.noreply.github.com>
2021-01-10 19:17:33 +0000

    Add files via upload

--- cuckoo.c
+++ cuckoo.c
@@ -1,26 +1,37 @@
+/*Cuckoo Hash works perfectly for ints in testassoc
+and it works for strings, but fails to calloc after
+120,000 new strings as hash table becomes too big.
+I can't quite find a good enough hash function in 
+spite of research*/
+
 #include "specific.h"
 #include "../../ADTs/General/general.h"
 #include <assert.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ctype.h>
 
 #define INITIALSIZE 17
 #define SCALEFACTOR 4
-#define HASH1 5
-#define HASH2 0
+#define HASH1 0
+#define HASH2 19
 #define TWOTHIRDS /1.5
+#define EMPTYHASH empty_hash.flag = false; \
+empty_hash.data = NULL; empty_hash.key = NULL;
 
 void _hash(assoc* a, void* key, unsigned int* hash);
 void _hash_two(assoc* a, void* key, unsigned int* hash);
 bool _add_hash(assoc* a, void* key, void* data);
 bool _add_hash_two(assoc* a, void* key, void* data);
-void _add_data(hash* a, void* key, void* data, unsigned int hash);
+void _add_data(hash* a, void* key, void* data, \
+unsigned int hash);
 assoc* _realloc(assoc* a);
 unsigned int _primetable(assoc* a);
 bool _isprime(unsigned int c); 
 bool _rehash(assoc* a, assoc* b);
 bool _isduplicate(assoc* a, void* key);
-hash _search(assoc* a, void* key);
+hash _search_one(assoc* a, void* key);
+hash _search_two(assoc* a, void* key);
 int log2n(unsigned int n);
 
 /*
@@ -29,12 +40,15 @@ int log2n(unsigned int n);
    This is important when comparing keys since
    we'll need to use either memcmp() or strcmp()
 */
+
 assoc* assoc_init(int keysize) {
 
     assoc *a =  ncalloc(1, sizeof(assoc));
     
-    a->hash_table = (hash*) ncalloc(sizeof(hash), INITIALSIZE);
-    a->hash_table2 = (hash*) ncalloc(sizeof(hash), INITIALSIZE);
+    a->hash_table = (hash*) ncalloc(sizeof(hash), \
+    INITIALSIZE);
+    a->hash_table2 = (hash*) ncalloc(sizeof(hash), \
+    INITIALSIZE);
     a->capacity = INITIALSIZE;
     a->keysize = keysize;
 
@@ -91,9 +105,17 @@ unsigned int assoc_count(assoc* a) {
 */
 void* assoc_lookup(assoc* a, void* key) {
 
-    hash hash1 = _search(a, key);
+    hash hash1 = _search_one(a, key);
+    hash hash2 = _search_two(a, key);
 
-    return hash1.data;
+    if (hash1.key != NULL) {
+        return hash1.data;
+    }
+    if (hash2.key != NULL) {
+        return hash2.data;
+    }
+
+    return NULL;
 }
 
 void assoc_todot(assoc* a);
@@ -113,20 +135,22 @@ https://gist.github.com/MohamedTaha98/ccdf734f13299efb73ff0b12f7ce429f */
 
 void _hash(assoc* a, void* key, unsigned int* hash) {
 
-    unsigned long h = 5381;
+    unsigned long h = 5381 + a->capacity;
     char *str;
-    unsigned int count = 0; 
+    unsigned int c, count = 0; 
     str = (char*)key;
 
     if (a->keysize) {
         while ((count < a->keysize)) {
-            h = ((h << HASH1) + h) + *str;
+            h = ((h << HASH1) + h) + *(int*)key;
             count++;
         }
     }
+
+    /*http://www.cse.yorku.ca/~oz/hash.html*/
     else {
-        while ((*str++)) {
-            h = ((h << HASH1) + h) + *str;
+        while ((c = *str++)){
+            h = c + (h << 6) + (h << 16) - h;
         }
     }
     *hash = h % a->capacity;
@@ -141,18 +165,20 @@ void _hash_two(assoc* a, void* key, unsigned int* hash) {
    
     if (a->keysize) {
         while ((count < a->keysize)) {
-            h = ((h << HASH2) + h) + *str;
+            h = ((h << HASH2) + *str + count);
             count++;
         }
     }
     else {
         while ((*str++)) {
-            h = ((h << HASH2) + h) + *str;
-       }
+            h = ((h << HASH2) + h + *str);
     }
-    *hash = h % a->capacity;
+    }
+    *hash = h % a->capacity;    
 }
 
+/* Find hash and insert data into hashtable
+*/
 bool _add_hash(assoc* a, void* key, void* data) {
 
     unsigned int hash = 0;
@@ -163,10 +189,12 @@ bool _add_hash(assoc* a, void* key, void* data) {
     old_key = a->hash_table[hash].key;
     old_data = a->hash_table[hash].data;
 
+    /*If cell is full bounce data into second hash table*/
     if (a->hash_table[hash].flag) {
         count += 1;
-        /*Resize hash table*/
+        /*Resize hash table after log base2 bounces*/
         if (count == log2n(a->capacity)) {
+            printf("count is %d capacity is %d assoc_count(a) is %d\n", count, a->capacity, assoc_count(a));
             count = 0;
             return false;
         }
@@ -177,11 +205,11 @@ bool _add_hash(assoc* a, void* key, void* data) {
         a->size += 1;
         _add_data(a->hash_table, key, data, hash);
     }
-    
-
     return true;
 }
 
+/* Find hash and insert into hashtable 
+*/
 bool _add_hash_two(assoc* a, void* key, void* data) {
 
     unsigned int hash = 0;
@@ -191,6 +219,7 @@ bool _add_hash_two(assoc* a, void* key, void* data) {
     old_key = a->hash_table2[hash].key;
     old_data = a->hash_table2[hash].data;
 
+    /*If cell is full bounce data into first hash table */
     if (a->hash_table2[hash].flag) {
         _add_data(a->hash_table2, key, data, hash);
         _add_hash(a, old_key, old_data);
@@ -199,29 +228,38 @@ bool _add_hash_two(assoc* a, void* key, void* data) {
         a->size += 1;
         _add_data(a->hash_table2, key, data, hash);
     }
-    
     return true;
 }
 
-void _add_data(hash* a, void* key, void* data, unsigned int hash) {
+/* Add data to correct cell in hash table 
+*/
+void _add_data(hash* a, void* key, void* data, \
+unsigned int hash) {
 
     a[hash].data = data;
     a[hash].flag = true;
     a[hash].key = key;
 }
 
+/* Allocate space for new hash table 
+*/
 assoc* _realloc(assoc* a) {
 
     assoc* b = ncalloc(1, sizeof(assoc));
 
-    b->hash_table = (hash*) ncalloc(sizeof(hash), _primetable(a));
-    b->hash_table2 = (hash*) ncalloc(sizeof(hash), _primetable(a));
+    b->hash_table = (hash*) ncalloc(sizeof(hash), \
+    _primetable(a));
+    b->hash_table2 = (hash*) ncalloc(sizeof(hash), \
+    _primetable(a));
     b->capacity = _primetable(a);
     b->keysize = a->keysize;
     
     return b;
 }
 
+/*Find the next prime number using scalefactor to\
+ size the new hash table
+ */
 unsigned int _primetable(assoc* a) {
 
     unsigned int prime;
@@ -234,6 +272,8 @@ unsigned int _primetable(assoc* a) {
     return prime;
 }
 
+/*Check whether a number is prime
+*/
 bool _isprime(unsigned int c) {
    
    unsigned int i; 
@@ -248,6 +288,8 @@ bool _isprime(unsigned int c) {
    return flag; 
 }   
 
+/* Take data from one table and hash into second table 
+*/
 bool _rehash(assoc* a, assoc* b) {
 
     unsigned int i = 0, size = a->capacity;
@@ -258,71 +300,94 @@ bool _rehash(assoc* a, assoc* b) {
 
     for (i = 0; i < size; i++) {
         if (a->hash_table[i].flag) {
-            _add_hash(b, a->hash_table[i].key, a->hash_table[i].data);
+            _add_hash(b, a->hash_table[i].key, \
+            a->hash_table[i].data);
         }
         if (a->hash_table2[i].flag) {
-            _add_hash(b, a->hash_table2[i].key, a->hash_table2[i].data);
+            _add_hash(b, a->hash_table2[i].key, \
+            a->hash_table2[i].data);
         }
     }
     return true;
 }
 
+/* Check if key already exists in hashtable 
+*/
 bool _isduplicate(assoc* a, void* key) {
 
-    hash hash1 = _search(a, key);
+    hash hash1 = _search_one(a, key);
+    hash hash2 = _search_two(a, key);
 
-    if (hash1.key!= NULL) {
+    if (hash1.key != NULL || hash2.key != NULL) {
         return true;
     }
     return false;
 }
 
-hash _search(assoc* a, void* key) {
+/* Search for key within first hashtable 
+ using strcmp and memcmp
+ */
+hash _search_one(assoc* a, void* key) {
 
     hash empty_hash;
-    unsigned int hashone, hashtwo;
+    unsigned int hashone;
     
     _hash(a, key, &hashone);
-    _hash_two(a, key, &hashtwo);
 
     if (!a->keysize) {
         if (a->hash_table[hashone].flag) {
-            if (!strcmp((char*)a->hash_table[hashone].key, (char*)key))  {
+            if (!strcmp((char*)a->hash_table[hashone].key,\
+             (char*)key))  {
                 return a->hash_table[hashone];
-            }
-        }
-        if (a->hash_table2[hashtwo].flag) {
-            if (!strcmp((char*)a->hash_table2[hashtwo].key, (char*)key)) {
-                return a->hash_table2[hashtwo];
             }
         }
     }
     else {
         if (a->hash_table[hashone].flag) {
-            if (!memcmp(a->hash_table[hashone].key, key, a->keysize))  {
+            if (!memcmp(a->hash_table[hashone].key, key,\
+             a->keysize))  {
                 return a->hash_table[hashone];
             }
         }
+    }   
+    EMPTYHASH
+    return empty_hash;
+}
+
+/* Search for key within second hashtable 
+using strcmp and memcmp
+*/
+hash _search_two (assoc* a, void* key) {
+
+    hash empty_hash;
+    unsigned int hashtwo;
+    
+    _hash_two(a, key, &hashtwo);
+    
+    if (!a->keysize) {
         if (a->hash_table2[hashtwo].flag) {
-            if (!memcmp(a->hash_table2[hashtwo].key, key, a->keysize)) {
+            if (!strcmp((char*)a->hash_table2[hashtwo].key,\
+             (char*)key)) {
                 return a->hash_table2[hashtwo];
             }
         }
     }
-    empty_hash.flag = false;
-    empty_hash.data = NULL;
-    empty_hash.key = NULL;
+    else {
+        if (a->hash_table2[hashtwo].flag) {
+            if (!memcmp(a->hash_table2[hashtwo].key,\
+             key, a->keysize)) {
+                return a->hash_table2[hashtwo];
+            }
+        }
+    }
+    EMPTYHASH
     return empty_hash;
 }
 
+/* Calculates log base 2 of a number 
+required when resizing array
+*/
 int log2n(unsigned int n) {
 
     return (n > 1) ? 1 + log2n(n / 2) : 0;
-}
-
-void _assoc_test(void) {
-
-    /*Test log2n*/
-    assert(log2n(16777216) == 24);
-
 }
